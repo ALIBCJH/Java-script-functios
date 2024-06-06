f181e5a5246cb1b8eed67e12313da129c54626cd
simonjuma_230 <simon.ombom22@students.dkut.ac.ke>
2024-06-06 23:38:58 +0300

    first commit

---
+++ idex.js
@@ -0,0 +1,10 @@
+//USING TERNARY OPERATORS FOR CONTROL STRUCTURE
+
+const name = "simon juma";
+//const FirstName = "rose achieng";
+age = 22;
+
+const voter = () => {
+  return age >= 18 ? "simon is allowed to marry" : "simon is too young";
+};
+console.log(voter());
