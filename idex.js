24621f48d7a5d076e2538a59e77c8e17775ba0e8
ALIGCJH <simonjuma465@gmail.com>
2025-01-20 14:44:05 +0300

    Add feature into idex.js

--- idex.js
+++ idex.js
@@ -8,3 +8,10 @@ const voter = () => {
   return age >= 18 ? "simon is allowed to marry" : "simon is too young";
 };
 console.log(voter());
+
+// Using ternary operator to test for a condition in javascript
+const marks = 40;
+const grade = () => {
+  return marks >= 40 ? "pass" : "fail";
+}
+console.log(grade());
